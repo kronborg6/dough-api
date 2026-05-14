f822817efe434284b138416bc1a0fb1137365fd7
Kronborg <mkronborg7@gmail.com>
2026-05-14 17:52:31 +0200

    made the database schema v1

--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -57,7 +57,7 @@ enum Recipe {
     DeletedAt,
 }
 #[derive(DeriveIden)]
-enum MerticType {
+enum MetricType {
     Table,
     Id,
     Name,
@@ -66,17 +66,17 @@ enum MerticType {
 enum RecipeMetric {
     Table,
     Id,
+    RecipeId,
     MetricTypeId,
     Value,
-    CreateAt,
+    CreatedAt,
+    UpdatedAt,
+    DeletedAt,
 }
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        // todo!();
-
         manager
             .create_table(
                 Table::create()
@@ -223,11 +223,98 @@ impl MigrationTrait for Migration {
                     .table(Recipe::Table)
                     .if_not_exists()
                     .col(pk_uuid(Recipe::Id))
+                    .col(uuid(Recipe::UserId).null())
                     .col(uuid(Recipe::Original).null())
-                    .col(uuid(Recipe::Original).null())
+                    .col(
+                        ColumnDef::new(Recipe::Visibility)
+                            .small_integer()
+                            .not_null(),
+                    )
                     .col(date_time(Recipe::CreatedAt).not_null())
                     .col(date_time(Recipe::UpdatedAt).not_null())
                     .col(date_time_null(Recipe::DeletedAt).null())
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(Recipe::Table, Recipe::UserId)
+                            .to(User::Table, User::Id)
+                            .on_delete(ForeignKeyAction::SetNull),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(Recipe::Table, Recipe::Original)
+                            .to(Recipe::Table, Recipe::Id)
+                            .on_delete(ForeignKeyAction::SetNull),
+                    )
+                    .check(Expr::col(Recipe::Visibility).between(0, 255))
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(MetricType::Table)
+                    .if_not_exists()
+                    .col(pk_uuid(MetricType::Id))
+                    .col(string(MetricType::Name).not_null())
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_index(
+                Index::create()
+                    .table(MetricType::Table)
+                    .name("idx-mertic-type-name")
+                    .col(MetricType::Name)
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(RecipeMetric::Table)
+                    .if_not_exists()
+                    .col(pk_uuid(RecipeMetric::Id))
+                    .col(uuid(RecipeMetric::MetricTypeId).not_null())
+                    .col(uuid(RecipeMetric::RecipeId).not_null())
+                    .col(string(RecipeMetric::Value).not_null())
+                    .col(date_time(RecipeMetric::CreatedAt).not_null())
+                    .col(date_time(RecipeMetric::UpdatedAt).not_null())
+                    .col(date_time_null(RecipeMetric::DeletedAt).null())
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(RecipeMetric::Table, RecipeMetric::RecipeId)
+                            .to(Recipe::Table, Recipe::Id)
+                            .on_delete(ForeignKeyAction::Cascade),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .from(RecipeMetric::Table, RecipeMetric::MetricTypeId)
+                            .to(MetricType::Table, MetricType::Id)
+                            .on_delete(ForeignKeyAction::Restrict),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_index(
+                Index::create()
+                    .table(RecipeMetric::Table)
+                    .name("idx-recipe-metric-mertic-type-id")
+                    .col(RecipeMetric::MetricTypeId)
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_index(
+                Index::create()
+                    .table(RecipeMetric::Table)
+                    .name("idx-recipe-mertic-recipe-id")
+                    .col(RecipeMetric::RecipeId)
                     .to_owned(),
             )
             .await?;
@@ -235,12 +322,12 @@ impl MigrationTrait for Migration {
         Ok(())
     }
 
-    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
         // Replace the sample below with your own migration scripts
-        todo!();
+        todo!()
 
-        manager
-            .drop_table(Table::drop().table("post").to_owned())
-            .await
+        // manager
+        //     .drop_table(Table::drop().table("post").to_owned())
+        //     .await
     }
 }
